af87c4d97246f48379e11ac18f6f32c064e14e2f
David Hayes <dhayes@lalgbtcenter.org>
2020-02-03 19:42:07 -0800

    add script to change default php version

---
+++ php_versions.sh
@@ -0,0 +1,5 @@
+sudo update-alternatives --set php /usr/bin/php7.3
+sudo update-alternatives --set phar /usr/bin/phar7.3
+sudo update-alternatives --set phar.phar /usr/bin/phar.phar7.3 
+sudo update-alternatives --set phpize /usr/bin/phpize7.3
+sudo update-alternatives --set php-config /usr/bin/php-config7.3
