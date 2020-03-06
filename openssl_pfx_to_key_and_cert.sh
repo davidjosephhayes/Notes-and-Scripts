bb8d4e51b86dd4aff5cd9d3b048e6bb244ed80d7
David Hayes <david@blackbrick.software>
2020-03-05 20:07:42 -0800

    add script to split pfx file to individual key/ca/cert

---
+++ openssl_pfx_to_key_and_cert.sh
@@ -0,0 +1,17 @@
+# https://www.markbrilman.nl/2011/08/howto-convert-a-pfx-to-a-seperate-key-crt-file/
+PFX_FILE=$1;
+OUTPUT=$2;
+
+if [ ! -f "$PFX_FILE" ] ; then
+   echo "Usage ./$0 PFX_FILE" >&2; exit 1
+fi
+
+PACKAGE="openssl"
+if ! dpkg -l | grep -q "$PACKAGE"; then
+  sudo apt install "$PACKAGE"
+fi
+
+openssl pkcs12 -in "$PFX_FILE" -nocerts -out "$PFX_FILE.key"
+openssl pkcs12 -in "$PFX_FILE" -nodes -nokeys -cacerts -out "$PFX_FILE.ca"
+openssl pkcs12 -in "$PFX_FILE" -clcerts -nokeys -out "$PFX_FILE.cert"
+
