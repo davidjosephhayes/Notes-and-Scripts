4c7d1fc776a23fdfc3beb590f432c1b284412653
David Hayes <dhayes@lalgbtcenter.org>
2020-03-06 12:46:12 -0800

    add a line to strip password from key file

--- openssl_pfx_to_key_and_cert.sh
+++ openssl_pfx_to_key_and_cert.sh
@@ -1,4 +1,6 @@
 # https://www.markbrilman.nl/2011/08/howto-convert-a-pfx-to-a-seperate-key-crt-file/
+# https://stackoverflow.com/questions/27497723/export-a-pkcs12-file-without-an-export-password
+
 PFX_FILE=$1;
 OUTPUT=$2;
 
@@ -11,7 +13,7 @@ if ! dpkg -l | grep -q "$PACKAGE"; then
   sudo apt install "$PACKAGE"
 fi
 
-openssl pkcs12 -in "$PFX_FILE" -nocerts -out "$PFX_FILE.key"
+openssl pkcs12 -in "$PFX_FILE" -nocerts -out "$PFX_FILE.pem"
+openssl rsa -in "$PFX_FILE.pem" -out "$PFX_FILE.key"
 openssl pkcs12 -in "$PFX_FILE" -nodes -nokeys -cacerts -out "$PFX_FILE.ca"
 openssl pkcs12 -in "$PFX_FILE" -clcerts -nokeys -out "$PFX_FILE.cert"
-
