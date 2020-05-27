0f2abe6dd8514522bdbe7ad22d291af48c8ba908
David Hayes <dhayes@lalgbtcenter.org>
2020-05-27 00:25:28 -0700

    update set default php version to do pear and pecl

--- php_set_default_version.sh
+++ php_set_default_version.sh
@@ -5,6 +5,22 @@ if ! [[ "$PHPVER" =~ $re ]] ; then
    echo "Usage ./$0 PHP_VERSION" >&2; exit 1
 fi
 
+PHPAPI=`php -i | grep 'PHP API' | sed -e 's/PHP API => //'`;
+
+# https://stackoverflow.com/questions/19561722/pecl-installs-for-previous-php-version
+
+if [ -x "$(command -v pecl)" ]; then
+   sudo pecl config-set ext_dir /usr/lib/php/$PHPAPI
+   sudo pecl config-set php_bin /usr/bin/php$PHPVER
+   sudo pecl config-set php_ini /etc/php/$PHPVER/cli/php.ini
+fi
+
+if [ -x "$(command -v pear)" ]; then
+   sudo pear config-set ext_dir /usr/lib/php/$PHPAPI
+   sudo pear config-set php_bin /usr/bin/php$PHPVER
+   sudo pear config-set php_ini /etc/php/$PHPVER/cli/php.ini
+fi
+
 sudo update-alternatives --set php /usr/bin/php$PHPVER;
 sudo update-alternatives --set phar /usr/bin/phar$PHPVER;
 sudo update-alternatives --set phar.phar /usr/bin/phar.phar$PHPVER;
