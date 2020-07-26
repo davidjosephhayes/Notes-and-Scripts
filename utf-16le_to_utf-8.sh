8d630284166275b0a8d037cb7d8566f436e83a9e
David Hayes <dhayes@lalgbtcenter.org>
2020-07-26 14:17:55 -0700

    add notes about utf16

--- utf-16le_to_utf-8.sh
+++ utf-16le_to_utf-8.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Generate Scripts to SQL out of SQL Server comes out in UTF-16 by default
+# You can pick out UTF-16LE files by a BOM at the beginning of the file FE FF
+# also, there will be a null 00 after evry ASCII character
+
 FILE=$0
 
 if [ ! -f "$FILE" ] ; then
